668e26fe4ed1fb476779a6704a3f0edddfd187d5
kemylly <kemylly@users.noreply.github.com>
2020-09-06 12:04:38 -0300

    Adicionei a direcao da cobra e aparecer novamente na tela

--- Cobrinha/Cobrinha/JS/Main.js
+++ Cobrinha/Cobrinha/JS/Main.js
@@ -24,9 +24,26 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+//37 direita 38 baixo 39 esquerda 40 pra cima
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 //funcao que atualiza o jogo e faca ela se mexer
 //ela faz parar o jogo
 function iniciarjogo() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
