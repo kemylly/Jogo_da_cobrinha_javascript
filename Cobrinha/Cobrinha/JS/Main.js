add7bbc7595a2f1d6b941c52f8cf72647b4c52f2
kemylly <kemylly@users.noreply.github.com>
2020-09-06 11:28:27 -0300

    Adicionei os primeiros movimentos da cobra

--- Cobrinha/Cobrinha/JS/Main.js
+++ Cobrinha/Cobrinha/JS/Main.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right"; //direcao da cobrinha
+
 function criarBG() { //tratar o background
     context.fillStyle = "LightPink"; //trabalhar com estilo
     context.fillRect(0, 0, 16 * box, 16 * box);  //desenhar o retangulo 4 paramentros
@@ -22,6 +24,32 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+//funcao que atualiza o jogo e faca ela se mexer
+//ela faz parar o jogo
+function iniciarjogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x; //posicao inicial
+    let snakeY = snake[0].y;
+
+    //direcao da cobrinha
+    if (direction == "right") snakeX += box; //incrementa 
+    if (direction == "left") snakeX -= box; //decrementa
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarjogo, 100); //renovar
+
+
 
